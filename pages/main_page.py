715e2ba3c1011ac7c7d7f7d204e7ce93d5a61a39
Turente <digitproo@mail.ru>
2025-02-26 13:46:33 +0300

    v0.3

---
+++ pages/main_page.py
@@ -0,0 +1,11 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+# создаем класс MainPage как наследник класса BasePage
+class MainPage(BasePage):
+    # в аргументах указываем аргумент self , чтобы иметь доступ к атрибутам и методам класса:
+    def go_to_login_page(self):
+        # Так как браузер у нас хранится как аргумент класса BasePage, обращаться к нему нужно соответствующим образом с помощью self
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link.click()
