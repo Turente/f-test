c72585d0fa64cb05cff0eebebdb16a8aa1e5e84d
Turente <digitproo@mail.ru>
2025-02-26 14:30:22 +0300

    v0.4

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,3 +9,8 @@ class MainPage(BasePage):
         # Так как браузер у нас хранится как аргумент класса BasePage, обращаться к нему нужно соответствующим образом с помощью self
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            By.CSS_SELECTOR, "#login_link"
+        ), "Login link is not presented"
