29b737419b6e739d4a625bf842366ea44e1ba8ef
Turente <digitproo@mail.ru>
2025-02-26 18:07:13 +0300

    v0.5

---
+++ pages/login_page.py
@@ -0,0 +1,27 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # проверка на корректный url адрес
+        current_url = self.browser.current_url
+        substring = "login"
+        assert substring in current_url, "There is no login URL"
+
+    def should_be_login_form(self):
+        # проверка, что есть форма логина
+        assert self.is_element_present(
+            *LoginPageLocators.LOGIN_FORM
+        ), "There is no login form"
+
+    def should_be_register_form(self):
+        # проверка, что есть форма регистрации на странице
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTER_FORM
+        ), "There is no register form"
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 
 # создаем класс MainPage как наследник класса BasePage
@@ -7,10 +7,11 @@ class MainPage(BasePage):
     # в аргументах указываем аргумент self , чтобы иметь доступ к атрибутам и методам класса:
     def go_to_login_page(self):
         # Так как браузер у нас хранится как аргумент класса BasePage, обращаться к нему нужно соответствующим образом с помощью self
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(
-            By.CSS_SELECTOR, "#login_link"
+            *MainPageLocators.LOGIN_LINK
         ), "Login link is not presented"
