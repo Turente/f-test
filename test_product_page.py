3f51156e6655a5de03cb33acf40497b49100bea9
Turente <digitproo@mail.ru>
2025-03-03 16:53:39 +0300

    v1.0

--- test_product_page.py
+++ test_product_page.py
@@ -19,7 +19,7 @@ class TestUserAddToBasketFromProductPage:
 
     def test_user_cant_see_success_message(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-        product_page = ProductPage(browser, link, 0)
+        product_page = ProductPage(browser, link)
         product_page.open()
         product_page.should_be_product_page()
         product_page.should_not_be_success_message()
@@ -92,3 +92,32 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_basket_page_empty()
+
+
+# проверки уведомлений
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    product_page = ProductPage(browser, link, 0)
+    product_page.open()
+    product_page.should_be_product_page()
+    product_page.add_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_not_be_success_message()
+
+
+def test_message_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    product_page = ProductPage(browser, link, 0)
+    product_page.open()
+    product_page.should_be_product_page()
+    product_page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    product_page = ProductPage(browser, link, 0)
+    product_page.open()
+    product_page.should_be_product_page()
+    product_page.add_to_basket()
+    product_page.solve_quiz_and_get_code()
+    product_page.should_be_disappeared_success_message()
